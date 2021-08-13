3413ccf37de4157267ff720a9cb645518aa0149f
Júlia Acosta <juliagabriela98@gmail.com>
2021-08-13 18:35:08 -0300

    add checkbox assert equals verification

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 The files in this repository uses python and selenium to test the html elements in the https://asuonline.asu.edu/ website
 
+Testing with selenium and unittest
+
 To run the simulation and tests: 
     python -m main
--- basePage.py
+++ basePage.py
@@ -17,18 +17,28 @@ class BasePage(unittest.TestCase):
     def checkbox1(self):
         firstInput = Select(self.driver.find_element_by_id("__BVID__72"))
 
-        options = ["Select degree type", "All degree", "Undergraduate", "Graduate", "Certificates"]
+        self.options = ["Select degree type", "All degree", "Undergraduate", "Graduate", "Certificates"]
         i = 0
 
         for option in firstInput.options:
-            self.assertEqual(option.text.lower(), options[i].lower())
-            print(option.text)
+            self.assertEqual(option.text.lower(), self.options[i].lower())
             i =  i + 1
 
         firstInput.select_by_value("undergraduate")
 
     def checkbox2(self):
         secondInput = Select(self.driver.find_element_by_id("__BVID__73"))
+        self.options = ["Select area of interest", "All interest area", "Art and design", "Business", "Communication and digital media",
+        "Computer science and technology", "Education", "Engineering", "Entrepreneurship and innovation", "Geographical sciences and urban planning",
+        "Health and wellness", "History", "Humanities", "Information technology", "Language", "Law, criminal justice and public service",
+        "Liberal arts", "Management", "Nursing", "Nutrition", "Psychology", "Science", "Social and behavioral sciences", "STEM", "Sustainability"]
+
+        i = 0
+
+        for option in secondInput.options:
+            self.assertEqual(option.text.lower(), self.options[i].lower())
+            i =  i + 1
+
         secondInput.select_by_value("engineering-degrees")
 
     def button(self):
