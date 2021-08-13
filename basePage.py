d76e45ec843fbf4dcf297c80d09d92e9390a6f4f
Júlia Acosta <juliagabriela98@gmail.com>
2021-08-13 20:02:41 -0300

    add check if element exists

--- basePage.py
+++ basePage.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 # Para manipular um elemento dropdown, declaramos ele como uma instância da classe Select
 from selenium.webdriver.support.select import Select
@@ -15,35 +16,49 @@ class BasePage(unittest.TestCase):
         self.driver.get("https://asuonline.asu.edu/")
 
     def checkbox1(self):
-        firstInput = Select(self.driver.find_element_by_id("__BVID__72"))
+        #Elemento existe?
+        try:
+            firstInput = Select(self.driver.find_element_by_id("__BVID__72"))
+            self.options = ["Select degree type", "All degree", "Undergraduate", "Graduate", "Certificates"]
+            i = 0
 
-        self.options = ["Select degree type", "All degree", "Undergraduate", "Graduate", "Certificates"]
-        i = 0
+            # Verifica se as opções definidas são as mesmas que o elemento contém
+            for option in firstInput.options:
+                self.assertEqual(option.text.lower(), self.options[i].lower())
+                i =  i + 1
 
-        for option in firstInput.options:
-            self.assertEqual(option.text.lower(), self.options[i].lower())
-            i =  i + 1
+            firstInput.select_by_value("undergraduate")
 
-        firstInput.select_by_value("undergraduate")
+        except NoSuchElementException:
+            print("Checkbox1 not found")
 
     def checkbox2(self):
-        secondInput = Select(self.driver.find_element_by_id("__BVID__73"))
-        self.options = ["Select area of interest", "All interest area", "Art and design", "Business", "Communication and digital media",
-        "Computer science and technology", "Education", "Engineering", "Entrepreneurship and innovation", "Geographical sciences and urban planning",
-        "Health and wellness", "History", "Humanities", "Information technology", "Language", "Law, criminal justice and public service",
-        "Liberal arts", "Management", "Nursing", "Nutrition", "Psychology", "Science", "Social and behavioral sciences", "STEM", "Sustainability"]
+        try:
+            secondInput = Select(self.driver.find_element_by_id("__BVID__73"))
+            self.options = ["Select area of interest", "All interest area", "Art and design", "Business", "Communication and digital media",
+            "Computer science and technology", "Education", "Engineering", "Entrepreneurship and innovation", "Geographical sciences and urban planning",
+            "Health and wellness", "History", "Humanities", "Information technology", "Language", "Law, criminal justice and public service",
+            "Liberal arts", "Management", "Nursing", "Nutrition", "Psychology", "Science", "Social and behavioral sciences", "STEM", "Sustainability"]
 
-        i = 0
+            i = 0
 
-        for option in secondInput.options:
-            self.assertEqual(option.text.lower(), self.options[i].lower())
-            i =  i + 1
+            # Verifica se as opções definidas são as mesmas que o elemento contém
+            for option in secondInput.options:
+                self.assertEqual(option.text.lower(), self.options[i].lower())
+                i =  i + 1
 
-        secondInput.select_by_value("engineering-degrees")
+            secondInput.select_by_value("engineering-degrees")
+
+        except NoSuchElementException:
+            print("Checkbox2 not found")
 
     def button(self):
-        exploreButton = self.driver.find_element_by_link_text("Explore degrees")
-        exploreButton.click()
+        try:
+            exploreButton = self.driver.find_element_by_link_text("Explore degrees")
+            exploreButton.click()
+        except NoSuchElementException:
+            print("Button not found")
+
 
     def test_exploringDegrees(self):
         self.checkbox1()
